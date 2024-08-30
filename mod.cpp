477a0d58a2795b6168627da70161bacd7759329e
TIP1 <ignatylimansky@gmail.com>
2024-08-30 14:31:38 +0300

    init

---
+++ mod.cpp
@@ -0,0 +1,28 @@
+name = "Tip's Bake House";
+author = "TIP219";
+/// Picture displayed from the Expansions menu
+picture = "media\pics\TipsBakery.paa";
+/// Display next to the item added by the mod
+logoSmall = "media\icons\bakehouse.paa";
+/// Logo displayed in the main menu
+logo = "media\pics\TipsBakery.paa";
+/// When the mouse is over, in the main menu
+logoOver = "media\icons\bakehouse.paa";
+actionName	= "GitHub";
+action = "https://github.com/TIP1";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "| Tip's BakeHouse | - Gamemode framework v1.0.0";
+/// Color used for DLC stripes and backgrounds
+dlcColor[] = 
+{
+	1, // Red
+	1, // Green
+	1, // Blue
+	1 // Alpha
+};
+/// Overview text, displayed from the extansion menu
+overview = "Hurry up, until its get cold!!";
+/// Hide the extansion name 
+hideName = 0;
+/// Hide the extansion menu
+hidePicture = 0;
